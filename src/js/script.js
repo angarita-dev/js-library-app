bf1e57b79618b1c02080a739bcfcc3c7b4d24f78
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-03 15:37:24 -0500

    Change identation in js

--- src/js/script.js
+++ src/js/script.js
@@ -19,23 +19,23 @@ function getBookData() {
 
 function render() {
   let bookList = '';
-  myLibrary.forEach((book, index) => { 
-                                      bookList += `<div class='bookInfo'>
-                                                      <div class='book' data-index='${index}'>
-                                                          <h2>Title: ${book.title} </h2>
-                                                          <h5>Author: ${book.author} </h5>
-                                                          <h5>Number of pages: ${book.numPages} </h5>
-                                                          <h5>Current state: ${book.read} </h5>
-                                                          <select name='read' class='book-read' onChange='changeRead(${index})'>
-                                                            <option value='none' selected disable hidden>Change read status</option>
-                                                            <option value='Read'>Read</option>
-                                                            <option value='Currently reading'>Currently reading</option>
-                                                            <option value='Not read'>Not read</option>
-                                                          </select>
-                                                          <button onClick=removeBook(${index}) class='delete-book-btn'>Delete</button>
-                                                      </div>
-                                                    </div>`
-                                        });
+  myLibrary.forEach((book, index) => {
+    bookList += `<div class='bookInfo'>
+                    <div class='book' data-index='${index}'>
+                        <h2>Title: ${book.title} </h2>
+                        <h5>Author: ${book.author} </h5>
+                        <h5>Number of pages: ${book.numPages} </h5>
+                        <h5>Current state: ${book.read} </h5>
+                        <select name='read' class='book-read' onChange='changeRead(${index})'>
+                          <option value='none' selected disable hidden>Change read status</option>
+                          <option value='Read'>Read</option>
+                          <option value='Currently reading'>Currently reading</option>
+                          <option value='Not read'>Not read</option>
+                        </select>
+                        <button onClick=removeBook(${index}) class='delete-book-btn'>Delete</button>
+                    </div>
+                  </div>`;
+  });
   document.getElementById('library').innerHTML = bookList;
 }
 
@@ -45,7 +45,7 @@ function addBookToLibrary() {
   render();
   document.getElementsByClassName('input')[0].style.display = 'none';
   document.getElementById('message').style.display = 'block';
-  document.getElementById('message').innerHTML = newBook.title + ' was added successfully !!!';
+  document.getElementById('message').innerHTML = `${newBook.title} was added successfully !!!`;
 }
 
 function newBook() {
