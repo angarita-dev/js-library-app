f36a2d0d0c3380080367c8e7a5c7906b1d4ce6a7
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-03 15:33:38 -0500

    JS Refactor

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@ function Book(title, author, numPages, read) {
   this.numPages = numPages;
   this.read = read;
 }
+
 function getBookData() {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
@@ -16,33 +17,35 @@ function getBookData() {
   return newBook;
 }
 
-function addBookToLibrary() {
-  let newBook = getBookData();
-  myLibrary.push(newBook);
-  render();
-  document.getElementsByClassName('input')[0].style.display = 'none'; 
-  document.getElementById('message').style.display = 'block';
-  document.getElementById('message').innerHTML = newBook.title + ' was added successfully !!!';
-}
-
 function render() {
   let bookList = '';
-  myLibrary.forEach((book, index) => { bookList += `<div class='bookInfo'>
-                                                 <div class='book' data-index='${index}'>
-                                                     <h2>Title: ${book.title} </h2>
-                                                     <h5>Author: ${book.author} </h5>
-                                                     <h5>Number of pages: ${book.numPages} </h5>
-                                                     <h5>Current state: ${book.read} </h5>
-                                                     <select name='read' class='book-read' onChange='changeRead(${index})'>
-                                                      <option value='none' selected disable hidden>Change read status</option>
-                                                      <option value='Read'>Read</option>
-                                                      <option value='Currently reading'>Currently reading</option>
-                                                      <option value='Not read'>Not read</option>
-                                                     </select>
-                                                    <button onClick=removeBook(${index}) class='delete-book-btn'>Delete</button>
-                                                 </div>
-                                                </div>`; }); 
+  myLibrary.forEach((book, index) => { 
+                                      bookList += `<div class='bookInfo'>
+                                                      <div class='book' data-index='${index}'>
+                                                          <h2>Title: ${book.title} </h2>
+                                                          <h5>Author: ${book.author} </h5>
+                                                          <h5>Number of pages: ${book.numPages} </h5>
+                                                          <h5>Current state: ${book.read} </h5>
+                                                          <select name='read' class='book-read' onChange='changeRead(${index})'>
+                                                            <option value='none' selected disable hidden>Change read status</option>
+                                                            <option value='Read'>Read</option>
+                                                            <option value='Currently reading'>Currently reading</option>
+                                                            <option value='Not read'>Not read</option>
+                                                          </select>
+                                                          <button onClick=removeBook(${index}) class='delete-book-btn'>Delete</button>
+                                                      </div>
+                                                    </div>`
+                                        });
   document.getElementById('library').innerHTML = bookList;
+}
+
+function addBookToLibrary() {
+  const newBook = getBookData();
+  myLibrary.push(newBook);
+  render();
+  document.getElementsByClassName('input')[0].style.display = 'none';
+  document.getElementById('message').style.display = 'block';
+  document.getElementById('message').innerHTML = newBook.title + ' was added successfully !!!';
 }
 
 function newBook() {
@@ -50,7 +53,7 @@ function newBook() {
   document.getElementById('message').style.display = 'none';
 }
 
-function removeBook(bookIndex){
+function removeBook(bookIndex) {
   myLibrary.splice(bookIndex, 1);
   render();
 }
